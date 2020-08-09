dd622733dca86301cb2fbe0f7cf5657b9c12931e
eggplantiny <eggplantiny@gmail.com>
2020-08-09 16:45:01 +0900

    grid 개발중

---
+++ src/components/molecules/VGrid/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+      class="v-grid"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VGrid',
+  props: {
+    mode: {
+      type: String,
+      default: 'list',
+      validator (value) {
+        return ['list', 'grid'].indexOf(value) !== -1
+      }
+    }
+  },
+  computed: {
+    classes () {
+      const { mode } = this
+      const result = {}
+      if (mode === 'list') {
+        result['v-grid-list'] = true
+      } else {
+        result['v-grid-grid'] = true
+      }
+      return result
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .v-grid {
+    width: 100%;
+    &-list {
+
+    }
+
+    &-grid {
+      display: grid;
+      grid-gap: 10px;
+      grid-auto-columns: 30%;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="container">
-    <template v-for="(post, index) in posts">
-      <v-card
-          :key="`${index}-post`"
-          class="mb-4"
-      >
-        <img :src="post.thumbnail" alt="" />
-        <v-card-title>
-          {{ post.title }}
-        </v-card-title>
-        <v-card-content>
-          {{ post.body }}
-        </v-card-content>
-      </v-card>
-    </template>
+    <v-grid mode="grid">
+      <template v-for="(post, index) in posts">
+        <v-card
+            :key="`${index}-post`"
+            class="mb-4"
+        >
+          <img :src="post.thumbnail" alt="" />
+          <v-card-title>
+            {{ post.title }}
+          </v-card-title>
+          <v-card-content>
+            {{ post.body }}
+          </v-card-content>
+        </v-card>
+      </template>
+    </v-grid>
     <infinite-loading spinner="waveDots" @infinite="infiniteHandler" />
   </div>
 </template>
@@ -24,10 +26,11 @@ import types from '@/store/types'
 import VCard from '@/components/molecules/VCard/index'
 import VCardTitle from '@/components/molecules/VCard/VCardTitle/Index'
 import VCardContent from '@/components/molecules/VCard/VCardContent/index'
+import VGrid from '@/components/molecules/VGrid/index'
 
 export default {
   name: 'Home',
-  components: { VCardContent, VCardTitle, VCard },
+  components: { VGrid, VCardContent, VCardTitle, VCard },
   computed: {
         posts () {
             return this.$store.getters[types.posts.getters.items]
